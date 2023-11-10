39592ddbc426cc5122ba9968b6699cb2165f44c4
Dániel Rácz <dracz898@gmail.com>
2023-11-10 11:18:57 +0100

    Add project files.

---
+++ EVVegijegyek/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EVVegijegyek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tanulo = @"..\..\..\Adatok.txt";
+            List<Tanulo> tanulok = new List<Tanulo>();
+            using (StreamReader reader = new StreamReader(tanulo))
+            {
+                string s = "";
+
+                while (!reader.EndOfStream)
+                {
+                    tanulok.Add(new Tanulo(reader.ReadLine()));
+                }
+            }
+        }
+    }
+}
---
+++ EVVegijegyek/Tanulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVVegijegyek
+{
+    class Tanulo
+    {
+        public string Neve { get; set; }
+        public string Oktatasiaz { get; set; }
+        public List<int> Jegyek; 
+
+        public Tanulo(string sor)
+        {
+            var adat = sor.Split("\t");
+            Neve = adat[0];
+            Oktatasiaz = adat[1];
+            Jegyek = adat[2];
+
+            //for (int i = 0; i < adat.Count(); i++)
+            //{
+            //    (adat[i]);
+            //}
+        }
+
+
+    }
+}
